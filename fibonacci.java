6bd7fb05cbb2f37000a00cae69c1854d1b9b0abb
Valentina Barrocas <176978933+valentinabarrocas@users.noreply.github.com>
2025-07-27 22:17:07 -0300

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,31 +1,19 @@
-
 public class fibonacci{
     public static int fibr(int n){
         //recursiva
         if (n==0) {return 0;}
-
         if (n==1){return 1;}
-
-        return fibr(n-1) + fibr(n-2);
-    }
-
+        return fibr(n-1) + fibr(n-2);}
     public static int fibl (int n){
         //linear
         if (n==0) return 0; if(n==1) return 1;
-
         int a = 0; int b = 1;
-
         for (int i = 2; i<= n; i++){
-            int temp=a+b;
-            a=b; b=temp;
-        }
-        return b;
-    }
-
+            int temp= a + b;
+            a = b; b = temp;}
+        return b;}
     public static void main (String[] args){
-        int n = 10; 
+        int n = 15; 
         System.out.println("fibonacci de "+ n + "é" + fibr(n));
-        System.out.println ("fibonacci de"+ n + "é" + fibl(n));
-    }
-
+        System.out.println ("fibonacci de"+ n + "é" + fibl(n));}
 }
